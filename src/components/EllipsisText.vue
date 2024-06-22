5ff315eea723ae7deb7c4bc8ada7520a2f18fa8b
hanzhenfang <hanzhenfffang@gmail.com>
2024-06-22 11:14:28 +0800

    fix: fix the startEllipsisLine props not work in some condition

--- src/components/EllipsisText.vue
+++ src/components/EllipsisText.vue
@@ -6,37 +6,54 @@ const props = withDefaults(
     suffix?: boolean;
     startEllipsisLine?: number;
     boundaryValue?: number;
+    rootDivId?: string;
   }>(),
   {
     startEllipsisLine: 1,
     suffix: false,
     boundaryValue: 0,
+    rootDivId: "app",
   }
 );
 
-let premitiveText: string = "";
+let primitiveText: string = "";
 const container = ref<HTMLDivElement>();
 const text = ref<HTMLSpanElement>();
-const cssEntirely = computed<boolean>(() => {
+const cssOnly = computed<boolean>(() => {
   return !props.suffix && props.startEllipsisLine === 1;
 });
 
 function autoElipsis(container: HTMLElement, textNode: HTMLSpanElement) {
-  const str = premitiveText;
+  const str = primitiveText;
   textNode.textContent = str;
   container.style.whiteSpace = "nowrap";
   container.style.width = "fit-content";
   const containerWidth = container.clientWidth;
   const parent = container.parentElement; // outer parents element
-  const parentWidth = parent!.clientWidth || parent!.offsetWidth;
-  if (containerWidth <= parentWidth) {
-    textNode.textContent = str;
-    return;
-  } else if (cssEntirely.value) {
+  if (!parent) return;
+  const parentWidth = parent.clientWidth || parent.offsetWidth;
+
+  const fontSize = getComputedStyle(textNode)["fontSize"];
+  textNode.style.height = parseFloat(fontSize) * props.startEllipsisLine + "px";
+
+  const ultimatelyHeight = container.clientHeight * props.startEllipsisLine; // use to estimate the finally result in startEllipsisLine prop is greater than 1
+
+  function justCssfn() {
     container.style.width = parentWidth + "px";
     container.style.whiteSpace = "nowrap";
     container.style.textOverflow = "ellipsis";
     container.style.overflow = "hidden";
+  }
+
+  if (parentWidth <= 0)
+    throw new Error(
+      "please wrap this component in a spec width father element.(请将该元素放置在一个指定准确宽度的父元素内部使用)"
+    );
+  if (containerWidth <= parentWidth) {
+    textNode.textContent = str;
+    return;
+  } else if (cssOnly.value) {
+    justCssfn();
     return;
   } else {
     const textWidth = textNode.offsetWidth;
@@ -45,43 +62,54 @@ function autoElipsis(container: HTMLElement, textNode: HTMLSpanElement) {
     const canFitStrNumber = Math.floor(
       (parentWidth * props.startEllipsisLine) / avgStrWidth
     );
-    const shouldDelNumber =
-      strNumer - canFitStrNumber + 1.5 + props.boundaryValue;
-    const delEachSide = shouldDelNumber / 2;
-    const endLeft = Math.floor(strNumer / 2 - delEachSide);
-    const startRight = Math.ceil(strNumer / 2 + delEachSide);
 
-    switch (props.suffix) {
-      case true: {
-        textNode.textContent =
-          str.slice(0, endLeft) + "..." + str.slice(startRight);
-        break;
-      }
-      case false: {
-        textNode.textContent = str.slice(0, -shouldDelNumber) + "...";
+    if (strNumer - canFitStrNumber >= 0) {
+      let flag = props.boundaryValue;
+      let containe_height = 0;
+      do {
+        const shouldDelNumber = strNumer - canFitStrNumber + 1.5 + flag;
+        const delEachSide = shouldDelNumber / 2;
+        const endLeft = Math.floor(strNumer / 2 - delEachSide);
+        const startRight = Math.ceil(strNumer / 2 + delEachSide);
 
-        break;
-      }
+        switch (props.suffix) {
+          case true: {
+            textNode.textContent =
+              str.slice(0, endLeft) + "..." + str.slice(startRight);
+            break;
+          }
+          case false: {
+            textNode.textContent = str.slice(0, -shouldDelNumber) + "...";
+            break;
+          }
+        }
+        container.style.wordBreak = "break-all";
+        container.style.whiteSpace = "normal";
+        containe_height = container.clientHeight;
+        flag++;
+      } while (containe_height > ultimatelyHeight);
+    } else {
+      justCssfn();
     }
-    container.style.wordBreak = "break-all";
-    container.style.whiteSpace = "normal";
   }
 }
 
 const observer = new ResizeObserver(() => {
-  const containerEl: HTMLDivElement =
-    container.value! || document.getElementById("autoEllipsisWrapper");
-  autoElipsis(containerEl, text.value!);
+  if (!container.value) return;
+  autoElipsis(container.value, text.value!);
 });
 
 nextTick(() => {
-  premitiveText = text.value?.innerText ?? "";
-  const app = document.getElementById("app");
-  observer.observe(app!);
+  if (!container.value) return;
+  primitiveText = text.value?.innerText ?? "";
+  if (primitiveText === "") return;
+  const app = document.getElementById(props.rootDivId);
+  if (!app) autoElipsis(container.value, text.value!);
+  else observer.observe(app);
 });
 </script>
 <template>
-  <div id="autoEllipsisWrapper" ref="container" v-bind="$attrs">
+  <div ref="container" v-bind="$attrs">
     <span ref="text">
       <slot />
     </span>
